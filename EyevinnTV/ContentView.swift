dc0438a039ad4556a5f1359c73a3d735e4ef32d3
SebastianLjungman <sebastian.ljungman@eyevinn.se>
2022-05-03 10:21:22 +0200

    Initial Commit

---
+++ EyevinnTV/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  EyevinnTV
+//
+//  Created by Sebastian Ljungman on 2022-05-03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ EyevinnTV/EyevinnTVApp.swift
@@ -0,0 +1,17 @@
+//
+//  EyevinnTVApp.swift
+//  EyevinnTV
+//
+//  Created by Sebastian Ljungman on 2022-05-03.
+//
+
+import SwiftUI
+
+@main
+struct EyevinnTVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
