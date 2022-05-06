0fe464e623dd63b120d6c24eb008b5dcd13a2a96
SebastianLjungman <sebastian.ljungman@eyevinn.se>
2022-05-06 13:29:01 +0200

    Support for reading local mRSS + loading live channels from mRSS Close #1 Close #3

--- EyevinnTV/ContentView.swift
+++ EyevinnTV/ContentView.swift
@@ -15,7 +15,14 @@ class videoURLObject: ObservableObject {
 
 struct ContentView: View {
     @State var videoItems = [Item]()
+    @State var liveVideoItems = [Item]()
     @StateObject var outerVideoURLObject = videoURLObject()
+
+    private var urlXmlVod = "https://testcontent.mrss.eyevinn.technology/"
+    private let localXmlVod = "testContent"
+    private let localXmlLive = "liveTestContent"
+    
+    private var readLocal = false
     
     var body: some View {
         NavigationView {
@@ -50,33 +57,20 @@ struct ContentView: View {
                     
                     ScrollView(.horizontal) {
                         HStack {
-                            VStack {
-                                Button {
-                                    outerVideoURLObject.currentVideoURL = "https://d2fz24s2fts31b.cloudfront.net/out/v1/6484d7c664924b77893f9b4f63080e5d/manifest.m3u8"
-                                    outerVideoURLObject.currentVideoTitle = "HLS LIVE"
-                                } label: {
-                                    VStack {
-                                        Image(systemName: "play.tv")
-                                            .font(.system(size: 100))
-                                        Text("HLS LIVE")
-                                            .bold()
-                                    }
-                                    .frame(width: 550, height: 250)
-                                }
-                            }
-                            
-                            VStack {
-                                Button {
-                                    outerVideoURLObject.currentVideoURL = "https://edfaeed9c7154a20828a30a26878ade0.mediatailor.eu-west-1.amazonaws.com/v1/master/1b8a07d9a44fe90e52d5698704c72270d177ae74/AdTest/master.m3u8"
-                                    outerVideoURLObject.currentVideoTitle = "HLS LIVE SSAI"
-                                } label: {
-                                    VStack {
-                                        Image(systemName: "play.tv")
-                                            .font(.system(size: 100))
-                                        Text("HLS LIVE SSAI")
-                                            .bold()
-                                    }
-                                    .frame(width: 550, height: 250)
+                            ForEach(liveVideoItems) { item in
+                                VStack {
+                                    Button {
+                                        outerVideoURLObject.currentVideoURL = item.videoURL
+                                        outerVideoURLObject.currentVideoTitle = item.title
+                                        print(item.videoURL) } label: {
+                                            VStack {
+                                                Image(systemName: "play.tv")
+                                                    .font(.system(size: 100))
+                                                Text("\(item.title)")
+                                                    .bold()
+                                            }
+                                            .frame(width: 550, height: 250)
+                                        }
                                 }
                             }
                         }
@@ -93,26 +87,51 @@ struct ContentView: View {
         }
     }
     
-    func loadData() {
-        let url = NSURL(string: "https://testcontent.mrss.eyevinn.technology/")
+    func parseXML(feed: String, isLive: Bool) {
+        let xml = SWXMLHash.parse(feed)
         
-        let task = URLSession.shared.dataTask(with: url! as URL) {(data, response, error) in
+        for elem in xml["feed"]["entry"].all
+        {
+            let item = Item()
+            item.title = elem["title"].element!.text
+            item.id = elem["id"].element!.text
+            item.videoURL = elem["link"].element!.text
+            isLive ? liveVideoItems.append(item) : videoItems.append(item)
+        }
+    }
+    
+    func loadData() {
+        if readLocal {
+            let xmlPath = Bundle.main.path(forResource: localXmlVod, ofType: "xml")
+            let data = NSData(contentsOfFile: xmlPath!)
+            
             if data != nil
             {
-                let feed=NSString(data: data!, encoding: String.Encoding.utf8.rawValue)! as String
-                let xml = SWXMLHash.parse(feed)
-                
-                for elem in xml["feed"]["entry"].all
-                {
-                    let item = Item()
-                    item.title = elem["title"].element!.text
-                    item.id = elem["id"].element!.text
-                    item.videoURL = elem["link"].element!.text
-                    videoItems.append(item)
-                }
+                let feed = NSString(data: data! as Data, encoding: String.Encoding.utf8.rawValue)! as String
+                parseXML(feed: feed, isLive: false)
             }
         }
-        task.resume()
+        
+        else {
+            let url = NSURL(string: urlXmlVod)
+            let task = URLSession.shared.dataTask(with: url! as URL) {(data, response, error) in
+                if data != nil
+                {
+                    let feed = NSString(data: data!, encoding: String.Encoding.utf8.rawValue)! as String
+                    parseXML(feed: feed, isLive: false)
+                }
+            }
+            task.resume()
+        }
+        
+        let xmlPath = Bundle.main.path(forResource: localXmlLive, ofType: "xml")
+        let data = NSData(contentsOfFile: xmlPath!)
+
+        if data != nil
+        {
+            let feed = NSString(data: data! as Data, encoding: String.Encoding.utf8.rawValue)! as String
+            parseXML(feed: feed, isLive: true)
+        }
     }
 }
 
