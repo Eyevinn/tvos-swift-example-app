61dd83db266ec71b63243438f096e21d6f6553c3
SebastianLjungman <sebastian.ljungman94@gmail.com>
2022-05-24 16:25:55 +0200

    fix: buildtime script now reads file URL:s directly
    Script updated to use the input file URL:s directly rather than their paths
    Added error handling

--- Scripts/CopyXml.swift
+++ Scripts/CopyXml.swift
@@ -7,9 +7,13 @@
 
 import Foundation
 
+enum fileReadError: Error {
+    case invalidFileUrl(String)
+}
+
 @main
 enum MyScript {
-    static func main() {
+    static func main() throws {
         guard let projectDir = ProcessInfo.processInfo.environment["SRCROOT"] else {
             print("SRCROOT is undefined")
             return
@@ -18,20 +22,21 @@ enum MyScript {
         let liveXml = (ProcessInfo.processInfo.environment["LIVE_XML"]) != nil ? (ProcessInfo.processInfo.environment["LIVE_XML"])!.replacingOccurrences(of: "\\", with: "") : nil
         let xmlsIndexed = [vodXml, liveXml].enumerated()
         
-        for (index, xml) in xmlsIndexed {
+        for (index, xmlUrl) in xmlsIndexed {
             let isLive = index == 1 ? true : false
-
-            if xml != nil, xml!.prefix(7) == "file://" {
-                let localFilePath = String(xml!.suffix(xml!.count - 7))
-                let data = NSData(contentsOfFile: localFilePath)
-
-                if data != nil {
-                    let filePath = projectDir + (isLive ? "/liveContentCopy.xml" : "/vodContentCopy.xml")
-                    if (FileManager.default.createFile(atPath: filePath, contents: data! as Data, attributes: nil)) {
-                        print("File created successfully.")
-                    } else {
-                        print("File not created.")
-                    }
+            
+            if xmlUrl != nil, xmlUrl!.prefix(7) == "file://" {
+                guard let data = NSData(contentsOf: URL(string: xmlUrl!)!) else {
+                    throw fileReadError.invalidFileUrl(xmlUrl!)
+                }
+                
+                let bundleFileUrl = URL(string: "file://" + projectDir + (isLive ? "/liveContentCopy.xml" : "/vodContentCopy.xml"))
+                do {
+                    try data.write(to: bundleFileUrl!)
+                    print("Content of file \(xmlUrl!) copied to file: \(bundleFileUrl!.absoluteURL)")
+                } catch {
+                    // Catch any errors
+                    print(error.localizedDescription)
                 }
             }
         }
